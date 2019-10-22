258fd3d9406e4a1ecbd845070a6a3206c77d8430
My Tran <myytran28@gmail.com>
2019-10-22 15:34:46 -0400

    add Navbar and Landing components and add bg image

---
+++ client/src/components/layouts/Landing.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Mane Street</h1>
+          <p className='lead'>
+            Calling all stylists and artists! create a profile/portfolio, share
+            posts and connect with other like minded artists near you!
+          </p>
+          <div className='buttons'>
+            <a href='register.html' className='btn btn-primary'>
+              Sign Up
+            </a>
+            <a href='login.html' className='btn btn-light'>
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
