2715a9dd0759d969d6b2fa914414e49821b4d34b
My Tran <myytran28@gmail.com>
2019-10-24 14:07:28 -0400

    add logout

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,7 +4,10 @@ import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -46,10 +49,7 @@ export const register = ({ name, email, password }) => async dispatch => {
       payload: res.data
     });
 
-    dispatch({
-      type: REGISTER_SUCCESS,
-      payload: res.data
-    });
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
 
@@ -60,4 +60,41 @@ export const register = ({ name, email, password }) => async dispatch => {
       type: REGISTER_FAIL
     });
   }
+};
+
+//Login user
+export const login = (email, password) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
+      type: LOGIN_FAIL
+    });
+  }
+};
+
+// Logout and clear the profile
+
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT });
 };
