e70f3bdebe21c45bfbdfe695aec9633ff1d98ffd
My Tran <myytran28@gmail.com>
2019-11-23 17:51:46 -0500

    add more pictures for landing page

--- client/src/components/layouts/Landing.js
+++ client/src/components/layouts/Landing.js
@@ -14,7 +14,8 @@ const insideStyles = {
   position: 'absolute',
   top: '50%',
   left: '50%',
-  transform: 'translate(-50%,-50%)'
+  transform: 'translate(-50%,-50%)',
+  opacity: 0.8
 };
 
 const Landing = ({ isAuthenticated }) => {
