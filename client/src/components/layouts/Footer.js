94ee9c12ad3afeb5b02c0401c4bf73a5490f0a14
My Tran <myytran28@gmail.com>
2019-11-25 14:10:31 -0500

    add copyright footer

--- client/src/components/layouts/Footer.js
+++ client/src/components/layouts/Footer.js
@@ -5,13 +5,18 @@ const Footer = () => {
     <div className='footer'>
       <div className='footer-content'>
         <div className='footer-content section'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-          unde enim numquam.
+          <h2>
+            <span>Mane</span>Studios
+          </h2>
+          <p>777 Mane Boulevard, Suite 777, Washington, DC, 20008</p>
         </div>
         <div className='footer-content section'>Lorem, ipsum.</div>
         <div className='footer-content section'>Lorem, ipsum dolor.</div>
       </div>
-      <div className='footer-bottom'>Copyrights</div>
+      <div className='footer-bottom'>
+        &copy; {new Date().getFullYear()} Wedding Networking App - All Rights
+        Reserved
+      </div>
     </div>
   );
 };
