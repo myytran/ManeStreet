d62f1b983a29883108791bdaf5e99ab737741b53
My Tran <myytran28@gmail.com>
2019-11-25 23:55:15 -0500

    add more Footer links

--- client/src/components/layouts/Footer.js
+++ client/src/components/layouts/Footer.js
@@ -5,15 +5,38 @@ const Footer = () => {
     <div className='footer'>
       <div className='footer-content'>
         <div className='footer-content section'>
-          <h2>
-            <span>Mane</span>Studios
-          </h2>
+          <h3>Mane Studios</h3>
           <p>777 Mane Boulevard, Suite 777, Washington, DC, 20008</p>
+          <p>
+            <i className='fas fa-phone'></i> 202-222-2222
+          </p>
+          <span>
+            <i className='fas fa-envelope'></i>&nbsp; mytranwebdesigns@gmail.com
+          </span>
         </div>
-        <div className='footer-content section'>Lorem, ipsum.</div>
-        <div className='footer-content section'>Lorem, ipsum dolor.</div>
+      </div>
+      <div className='footer-content section'>
+        <h3>Mane Studios</h3>
+        <p>777 Mane Boulevard, Suite 777, Washington, DC, 20008</p>
+        <p>
+          <i className='fas fa-phone'></i> 202-222-2222
+        </p>
+        <span>
+          <i className='fas fa-envelope'></i>&nbsp; mytranwebdesigns@gmail.com
+        </span>
+      </div>
+      <div className='footer-content section'>
+        <h3>Mane Studios</h3>
+        <p>777 Mane Boulevard, Suite 777, Washington, DC, 20008</p>
+        <p>
+          <i className='fas fa-phone'></i> 202-222-2222
+        </p>
+        <span>
+          <i className='fas fa-envelope'></i>&nbsp; mytranwebdesigns@gmail.com
+        </span>
       </div>
       <div className='footer-bottom'>
+        <h3>My Tran</h3>
         &copy; {new Date().getFullYear()} Wedding Networking App - All Rights
         Reserved
       </div>
