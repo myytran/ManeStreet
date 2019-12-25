f335dbe5c9e484948c7729b5d2380bea1f793666
My Tran <myytran28@gmail.com>
2019-12-25 17:57:20 -0500

    properly align footer items

--- client/src/components/layouts/Footer.js
+++ client/src/components/layouts/Footer.js
@@ -4,10 +4,13 @@ const Footer = () => {
   return (
     <div className='footer'>
       <div className='footer-content'>
-        <div className='copyright'>
-          <h4>My Tran</h4>
-          &copy; {new Date().getFullYear()} Beauty Networking App - My Tran ||
-          All Rights Reserved
+        &copy; {new Date().getFullYear()} Beauty Networking App - My Tran || All
+        Rights Reserved
+        <div className='footer-content-2'>
+          <h6>Social</h6>
+          <i class='fab fa-facebook-square'></i>
+          <i class='fas fa-share-alt-square'></i>
+          <i class='fas fa-envelope'></i>
         </div>
       </div>
     </div>
